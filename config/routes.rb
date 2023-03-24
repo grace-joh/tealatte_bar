fa1504fd8777e6923e1c00b52a0459c941c2a5aa
Grace Joh <graceehjoh@gmail.com>
2023-03-23 23:11:05 -0500

    refactor: routes with resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/categories', to: 'categories#index'
-  get '/categories/:id', to: 'categories#show'
-  get '/categories/new', to: 'categories#new'
-  post '/categories', to: 'categories#create'
-  get '/categories/:id/edit', to: 'categories#edit'
-  patch '/categories/:id', to: 'categories#update'
+  # get '/categories', to: 'categories#index'
+  # get '/categories/:id', to: 'categories#show'
+  # get '/categories/new', to: 'categories#new'
+  # post '/categories', to: 'categories#create'
+  # get '/categories/:id/edit', to: 'categories#edit'
+  # patch '/categories/:id', to: 'categories#update'
+  resources :categories, except: [:destroy]
 
   get '/categories/:id/drinks', to: 'category_drinks#index'
   get '/categories/:id/drinks/new', to: 'category_drinks#new'
   post '/categories/:id/drinks/new', to: 'category_drinks#create'
-  # resources :categories do
-  #   resources :drinks, only: [:index, :new, :create], controller: 'category_drinks'
-  # end
+  # resources :category_drinks, only: [:index, :new, :create]
 
-  get '/drinks', to: 'drinks#index'
-  get '/drinks/:id', to: 'drinks#show'
-  get '/drinks/:id/edit', to: 'drinks#edit'
-  patch '/drinks/:id', to: 'drinks#update'
-
-
+  # get '/drinks', to: 'drinks#index'
+  # get '/drinks/:id', to: 'drinks#show'
+  # get '/drinks/:id/edit', to: 'drinks#edit'
+  # patch '/drinks/:id', to: 'drinks#update'
+  resources :drinks, except: [:new, :create, :destroy]
 end
