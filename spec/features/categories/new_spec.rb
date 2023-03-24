fb498e201895ac41d193589dc89a64a44ecdccf1
Grace Joh <graceehjoh@gmail.com>
2023-03-24 09:06:35 -0500

    refactor: Change view creation form button text to Add Category and update test

--- spec/features/categories/new_spec.rb
+++ spec/features/categories/new_spec.rb
@@ -1,20 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'the category new page' do
-  it 'links to the new page from the category index' do
-    visit '/categories'
+  before(:each) do
+    visit '/categories/new'
+  end
 
-    click_link('New Category')
-    expect(current_path).to eq('/categories/new')
+  it 'displays the creation form' do
+    expect(page).to have_field('Name')
+    expect(page).to have_field('Price')
+    expect(page).to have_content('Caffeinated')
+    expect(page).to have_unchecked_field('True')
+    expect(page).to have_unchecked_field('False')
+    expect(page).to have_button('Add Category')
   end
 
   it 'can create a new category' do
-    visit '/categories/new'
-
     fill_in('Name', with: 'Fruit Teas')
     fill_in('Price', with: 5)
     choose 'caffeinated_true'
-    click_button('Create Category')
+    click_button('Add Category')
 
     expect(current_path).to eq('/categories')
     expect(page).to have_content('Fruit Teas')
