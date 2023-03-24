ace59f653438b745e62b2118a41d69e8fd9a23f8
Grace Joh <graceehjoh@gmail.com>
2023-03-24 09:08:13 -0500

    Add/test edit button to each category on index page (User Story 17)

--- spec/features/categories/index_spec.rb
+++ spec/features/categories/index_spec.rb
@@ -4,20 +4,30 @@ RSpec.describe 'the categories index page', type: :feature do
   before(:each) do
     @milk_teas = Category.create!(name: 'Milk Teas', price: 5, caffeinated: true)
     @smoothies = Category.create!(name: 'Smoothies', price: 7, caffeinated: false)
+  
+    visit '/categories'
   end
 
-  it 'displays the names of all categories and created at timestamp' do
-    visit '/categories'
+  it 'displays the page title' do
+    expect(page).to have_content('Menu')
+  end
 
+  it 'displays the names of all categories with edit link and created at timestamp' do
     expect(page).to have_content(@milk_teas.name)
+    expect(page).to have_link('Edit', href: "/categories/#{@milk_teas.id}/edit")
     expect(page).to have_content("Created at #{@milk_teas.created_at}")
     expect(page).to have_content(@smoothies.name)
+    expect(page).to have_link('Edit', href: "/categories/#{@smoothies.id}/edit")
     expect(page).to have_content("Created at #{@smoothies.created_at}")
   end
 
   it 'displays the category names sorted by newest category' do
-    visit '/categories'
-
     expect(@smoothies.name).to appear_before(@milk_teas.name)
+  end
+
+  it 'links to the new page from the category index' do
+    click_link('Add a category')
+
+    expect(current_path).to eq('/categories/new')
   end
 end
