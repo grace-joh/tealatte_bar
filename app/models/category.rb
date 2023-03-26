29756c0881f6687ca1102cdc870576cf799f1cc9
Grace Joh <graceehjoh@gmail.com>
2023-03-26 11:36:38 -0500

    Add #sorted_and_filtered_drinks to Category model and add test

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,12 @@ class Category < ApplicationRecord
   def self.sort_by_newest
     order(created_at: :desc)
   end
+
+  def sorted_and_filtered_drinks(cal_max, sort)
+    drinks = self.drinks
+    drinks = drinks.order(:name) if sort == 'abc'
+    drinks = drinks.order(:calories) if sort == 'cal'
+    drinks = drinks.where('calories <= ?', cal_max.to_i) if cal_max.present?
+    drinks
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe Category do
     @lemonade = Category.create!(name: 'Lemonade', price: 5, caffeinated: false)
     @black_mt = Drink.create!(name: 'Black Milk Tea', calories: 270, has_milk: true, category_id: @milk_teas.id)
     @oolong_mt = Drink.create!(name: 'Oolong Milk Tea', calories: 230, has_milk: true, category_id: @milk_teas.id)
+    @green_mt = Drink.create!(name: 'Green Milk Tea', calories: 220, has_milk: true, category_id: @milk_teas.id)
+    @hongkong_mt = Drink.create!(name: 'Hong Kong Milk Tea', calories: 300, has_milk: true, category_id: @milk_teas.id)
     @mango_smth = Drink.create!(name: 'Mango Smoothie', calories: 300, has_milk: true, category_id: @smoothies.id)
   end
 
@@ -17,8 +19,34 @@ RSpec.describe Category do
   describe 'instance methods' do
     describe '#drinks' do
       it 'returns all of the drink objects in the category' do
-        expect(@milk_teas.drinks).to eq([@black_mt, @oolong_mt])
-        expect(@milk_teas.drinks.count).to eq(2)
+        expect(@milk_teas.drinks).to eq([@black_mt, @oolong_mt, @green_mt, @hongkong_mt])
+        expect(@milk_teas.drinks.count).to eq(4)
+      end
+    end
+
+    describe '#sorted_and_filtered_drinks' do
+      it 'returns drinks under max calories sorted alphabetically' do
+        expect(@milk_teas.sorted_and_filtered_drinks(280, 'abc')).to eq([@black_mt, @green_mt, @oolong_mt])
+      end
+
+      it 'returns drinks under max calories sorted by calories ascending' do
+        expect(@milk_teas.sorted_and_filtered_drinks(280, 'cal')).to eq([@green_mt, @oolong_mt, @black_mt])
+      end
+
+      it 'returns drinks under max calories not sorted' do
+        expect(@milk_teas.sorted_and_filtered_drinks(280, false)).to eq([@black_mt, @oolong_mt, @green_mt])
+      end
+
+      it 'returns all drinks sorted by name alphabetically' do
+        expect(@milk_teas.sorted_and_filtered_drinks(nil, 'abc')).to eq([@black_mt, @green_mt, @hongkong_mt, @oolong_mt])
+      end
+
+      it 'returns all drinks sorted by calories ascending' do
+        expect(@milk_teas.sorted_and_filtered_drinks(nil, 'cal')).to eq([@green_mt, @oolong_mt, @black_mt, @hongkong_mt])
+      end
+
+      it 'returns all drinks unsorted and unfiltered' do
+        expect(@milk_teas.sorted_and_filtered_drinks(nil, false)).to eq([@black_mt, @oolong_mt, @green_mt, @hongkong_mt])
       end
     end
   end
